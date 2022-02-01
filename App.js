7c17bb50237fe7c3fad496f1746433cc925ef342
Sami Mäkelä <mrsmkl@gmail.com>
2022-02-01 17:13:57 +0200

    this is very retarded

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import 'react-native-get-random-values'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import nvm from '@nevermined-io/nevermined-sdk-js';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{new nvm.utils}</Text>
       <StatusBar style="auto" />
     </View>
   );
