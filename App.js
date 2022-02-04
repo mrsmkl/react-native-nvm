e42c02cceff7afea55a5d77d1a7c816036f37906
Sami Mäkelä <mrsmkl@gmail.com>
2022-02-04 12:51:18 +0200

    adding config files

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import 'react-native-get-random-values'
+import './shim.js'
+import crypto from 'crypto'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import nvm from '@nevermined-io/nevermined-sdk-js';
+import { zeroX, KeyTransfer } from "@nevermined-io/nevermined-sdk-js/dist/node/utils";
+
+import { NativeModules } from 'react-native'
+const { RNRandomBytes } = NativeModules
+
+// NativeModules.RNRandomBytes = {}
+/*
+RNRandomBytes.randomBytes(32, (err, bytes) => {
+  // bytes is a base64string
+})
+*/
 
 export default function App() {
+  const keyTransfer = new KeyTransfer()
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>{new nvm.utils}</Text>
+      <Text>{zeroX("12334")}</Text>
       <StatusBar style="auto" />
     </View>
   );
