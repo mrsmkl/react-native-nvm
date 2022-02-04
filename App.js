20a8c0b9fd56bc9b09853ea70380ddfd56fae7d1
Sami Mäkelä <mrsmkl@gmail.com>
2022-02-04 12:58:03 +0200

    cleanup

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
 import './shim.js'
-import crypto from 'crypto'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import nvm from '@nevermined-io/nevermined-sdk-js';
-import { zeroX, KeyTransfer } from "@nevermined-io/nevermined-sdk-js/dist/node/utils";
-
-import { NativeModules } from 'react-native'
-const { RNRandomBytes } = NativeModules
-
-// NativeModules.RNRandomBytes = {}
-/*
-RNRandomBytes.randomBytes(32, (err, bytes) => {
-  // bytes is a base64string
-})
-*/
+import { zeroX } from "@nevermined-io/nevermined-sdk-js/dist/node/utils";
 
 export default function App() {
-  const keyTransfer = new KeyTransfer()
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
